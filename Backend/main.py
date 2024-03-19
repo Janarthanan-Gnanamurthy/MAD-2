c6b9a081d019c5d1f8034837f6740a9420a53c34
Janarthanan-Gnanamurthy <janagnanam12@gmail.com>
2024-03-19 23:42:05 +0530

    added sqlite

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,44 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Define your models
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+# Assuming you've imported db from your models or database configuration file
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/home', methods=['GET'])
 def root():
     return {'message': 'success'}
+
+
+def create_user(username, email):
+    new_user = User(username=username, email=email)
+    db.session.add(new_user)
+    db.session.commit()
+    return new_user
+
+
+def get_users():
+    return User.query.all()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
