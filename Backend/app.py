48a3fdf6af5d109aeb80a630d937e90333fce871
Janarthanan-Gnanamurthy <janagnanam12@gmail.com>
2024-03-20 23:24:44 +0530

    renamed flask file

--- Backend/app.py
+++ Backend/app.py
@@ -16,6 +16,7 @@ def root():
     return {'message': 'success'}
 
 
+@app.post('/create-user')
 def create_user(username, email):
     new_user = User(username=username, email=email)
     db.session.add(new_user)
