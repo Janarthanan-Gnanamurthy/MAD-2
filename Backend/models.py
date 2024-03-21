44d8ca206e9e02949a50c07a3ae107c2a0ce72a4
Janarthanan-Gnanamurthy <janagnanam12@gmail.com>
2024-03-21 22:39:55 +0530

    added Books table

--- Backend/models.py
+++ Backend/models.py
@@ -7,3 +7,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    date_issued = db.Column(db.Date)
+    return_date = db.Column(db.Date)
