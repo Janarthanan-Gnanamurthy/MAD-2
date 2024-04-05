24ff2b70347f911621442b452fa6e60c8dd0023a
Janarthanan-Gnanamurthy <janagnanam12@gmail.com>
2024-04-05 23:41:07 +0530

    fixed book view page

--- Frontend/src/views/BookView.vue
+++ Frontend/src/views/BookView.vue
@@ -1,5 +1,45 @@
 <template>
-    <main>
-        this is BOOk page
-    </main>
+  <div>
+    <iframe :src="pdfUrl" width="100%" height="500"></iframe>
+  </div>
 </template>
+
+<script>
+export default {
+	props: {
+		book_id: {
+			type: Number,
+      required: true				
+		}
+	},
+  data() {
+    return {
+      pdfUrl: '', // Replace with logic to get your PDF URL
+    }
+  },
+	async mounted(){
+		await this.fetchBook()
+	},
+	methods: {
+		async fetchBook(){
+			try{
+				let response = await fetch(`http://localhost:5000/get-book/${this.book_id}`, {
+					method: 'GET',
+					headers: {
+						Authorization: `Bearer ${this.$store.state.token}`
+					}
+				})
+				if (response.ok){
+					let data = await response.json()
+					console.log(data)
+					this.pdfUrl = data.pdfurl
+				}
+
+
+			}catch (error) {
+				console.log("error While book:", error)
+			}
+		}
+	}
+}
+</script>
