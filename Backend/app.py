b3a742642490e4db9b188e6f3a19cf232f8755b2
Janarthanan-Gnanamurthy <janagnanam12@gmail.com>
2024-04-12 21:25:52 +0530

    Created UserBooks model

--- Backend/app.py
+++ Backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from flask_restful import Api
 from api import UserResource, SectionResource, BookResource, AdminRequestsResource
 
-from models import db, User, Request, Book, user_books
+from models import db, User, Request, Book, UserBooks
 
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from flask_cors import CORS
@@ -105,27 +105,31 @@ def approve_book_request():
     return jsonify({'message': 'Request approved successfully'}), 200
 
 
-@app.route('/admin/revoke/<int:request_id>', methods=['POST'])
+@app.route('/admin/revoke', methods=['POST'])
 @jwt_required()
-def reject_book_request(request_id):
+def reject_book_request():
     user_id = get_jwt_identity()['id']
     response = request.get_json()
-    request = Request.query.get(response.request_id)
-    if not request:
+    db_request = Request.query.get(response['request_id'])
+    if not db_request:
         return jsonify({'message': 'Request not found'}), 404
-    request.status = 'Revoked'
+    db_request.status = 'Revoked'
 
-    user_book = db.session.query(user_books).filter_by(
-        user_id=user_id, book_id=response.book_id).first()
-    db.session.delete(user_book)
-    db.session.commit()
+    user_book = db.session.query(UserBooks).filter_by(
+        user_id=user_id, book_id=response['book_id']
+    ).first()
 
-    return jsonify({'message': 'Request rejected successfully'}), 200
+    if user_book:
+        db.session.delete(user_book)
+        db.session.commit()
+        return jsonify({'message': 'Access Revoked successfully'}), 200
+    else:
+        return jsonify({'message': 'User book not found'}), 404
 
 
 @app.route('/user/books', methods=['GET'])
 @jwt_required()
-def get_user_books():
+def get_UserBooks():
     user_id = get_jwt_identity()['id']
     user = User.query.get(user_id)
 
@@ -138,7 +142,7 @@ def get_user_books():
             'name': book.name
         }
 
-        user_book = db.session.query(user_books).filter_by(
+        user_book = db.session.query(UserBooks).filter_by(
             user_id=user_id, book_id=book.id).first()
 
         if user_book.returned_on:
@@ -171,7 +175,7 @@ def has_book_access(func):
             return jsonify({'error': 'Invalid user'}), 401
 
         book = Book.query.get(book_id)
-        user_book = db.session.query(user_books).filter_by(
+        user_book = db.session.query(UserBooks).filter_by(
             user_id=current_user_id, book_id=book_id).first()
 
         if book and user_book and user_book.returned_on is None:
@@ -198,7 +202,7 @@ def get_book_content(book_id):
 @jwt_required()
 def return_book(book_id):
     user_id = get_jwt_identity()['id']
-    db.session.query(user_books).filter_by(
+    db.session.query(UserBooks).filter_by(
         user_id=user_id, book_id=book_id).update({'returned_on': date.today()})
     Request.query.filter_by(user_id=user_id, book_id=book_id).update(
         {'status': 'Completed'})
@@ -213,7 +217,7 @@ def feedback(book_id):
     print(response)
     user_id = get_jwt_identity()['id']
 
-    result = db.session.query(user_books).filter_by(
+    result = db.session.query(UserBooks).filter_by(
         user_id=user_id, book_id=book_id
     ).update(
         {'feedback': response['feedback']}, synchronize_session=False
