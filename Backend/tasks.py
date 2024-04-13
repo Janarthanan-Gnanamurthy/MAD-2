56ef9037ff5e0bb88a62c62dfc8e93a34b284e18
Janarthanan-Gnanamurthy <janagnanam12@gmail.com>
2024-04-13 22:25:59 +0530

    added celery worker

--- Backend/tasks.py
+++ Backend/tasks.py
@@ -12,22 +12,28 @@ load_dotenv()
 def send_daily_reminder():
     # Get the last 24 hours
     last_24_hours = datetime.now() - timedelta(days=1)
-
     # Get a list of users who haven't visited the app in the last 24 hours
     users_to_remind = User.query.filter(
         User.last_visited < last_24_hours).all()
-
     # Send reminders to each user
     for user in users_to_remind:
         send_reminder(user)
 
 
 def send_reminder(user):
-    subject = "Daily Reminder: Visit the App"
+    subject = f"Daily Reminder: Hey {user.username}, we are missing you!"
     recipients = [user.email]
-    body = f"Hi Sangeetha,\n\nThis is a friendly reminder to visit our app today. We hope you're having a great day!\n\nBest regards,\nThe App Team"
+    body = f"Hi {user.username},\n\nThis is a friendly reminder to visit our app today. We hope you're having a great day!\n\nBest regards,\nThe App Team"
 
     yag = yagmail.SMTP(user=os.environ.get('MAIL_DEFAULT_SENDER'),
                        password=os.environ.get('MAIL_PASSWORD'))
-    yag.send(to=recipients, subject=subject, contents=body)
+    try:
+        yag.send(to=recipients, subject=subject, contents=body)
+    except yagmail.errors.YagmailSMTPError as e:
+        print(f"Error sending email: {e}")
+    except yagmail.errors.YagmailInvalidArguments as e:
+        print(f"Invalid arguments provided: {e}")
+
+    print(f'Sent Mail to User: {user.username}')
+
     return {'message': {f'sent Mail to all User: {user.username}'}}
